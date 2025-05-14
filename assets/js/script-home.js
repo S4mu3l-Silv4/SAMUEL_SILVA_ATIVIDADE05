40bf1f3bb5471b2b7e539791f701365b0eba455b
S4mu3l-Silv4 <samuelssone02@gmail.com>
2025-05-14 11:16:54 -0300

    Adicionando menu hambúguer em telas mobile

--- assets/js/script-home.js
+++ assets/js/script-home.js
@@ -15,6 +15,52 @@ window.addEventListener("scroll", function () {
     lastScrollTop = scrollTop <= 0 ? 0 : scrollTop
 })
 
+// Menu hambúrguer em telas versão mobile:
+
+class MobileNavbar {
+    constructor(mobileMenu, navList, navLinks) {
+        this.mobileMenu = document.querySelector(mobileMenu)
+        this.navList = document.querySelector(navList)
+        this.navLinks = document.querySelectorAll(navLinks)
+        this.activeClass = "active"
+        this.handleClick = this.handleClick.bind(this)
+    }
+
+    animateLinks() {
+        this.navLinks.forEach((link, index) => {
+            link.style.animation
+                ? (link.style.animation = "")
+                : (link.style.animation = `navLinkFade 0.5s ease forwards ${
+                    index / 7 + 0.3
+                }s`)
+        })
+    }
+
+    handleClick() {
+        this.navList.classList.toggle(this.activeClass)
+        this.mobileMenu.classList.toggle(this.activeClass)
+        this.animateLinks()
+    }
+
+    addClickEvent() {
+        this.mobileMenu.addEventListener("click", this.handleClick)
+    }
+
+    init() {
+        if (this.mobileMenu) {
+            this.addClickEvent()
+        }
+        return this
+    }
+}
+
+const mobileNavbar = new MobileNavbar(
+    ".mobile-menu",
+    ".nav-list",
+    ".nav-list li",
+)
+mobileNavbar.init()
+
 // Carrossel de imagens:
 
 const carrossel = document.querySelectorAll('.slider')
