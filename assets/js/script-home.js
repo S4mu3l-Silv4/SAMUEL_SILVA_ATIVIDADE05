aff19140daeebf87530f2212ab88b3e58e8ff072
S4mu3l-Silv4 <samuelssone02@gmail.com>
2025-06-03 14:35:43 -0300

    Teste

--- assets/js/script-home.js
+++ assets/js/script-home.js
@@ -1,101 +1,101 @@
 // Nav bar com scroll inteligente:
 
-let ultimoScrollTop = 0
-const navBar = document.getElementsByClassName("nav-bar")
+    let ultimoScrollTop = 0
+    const navBar = document.getElementById("nav-bar")
 
-window.addEventListener("scroll", function () {
-    const scrollTop = window.pageYOffset || document.documentElement.scrollTop
+    window.addEventListener("scroll", function () {
+        const scrollTop = window.pageYOffset || document.documentElement.scrollTop
 
-    if (scrollTop > ultimoScrollTop) {
-        navBar.style.top = "-55px"
-    } else {
-        navBar.style.top = "0"
-    }
+        if (scrollTop > ultimoScrollTop) {
+            navBar.style.top = "-55px"
+        } else {
+            navBar.style.top = "0"
+        }
 
-    ultimoScrollTop = scrollTop <= 0 ? 0 : scrollTop
-})
+        ultimoScrollTop = scrollTop <= 0 ? 0 : scrollTop
+    })
 
 // Nav bar hambúrguer em telas mobile:
 
-class navBarMobile {
-    constructor(menuMobile, listaNav, linksNav) {
-        this.menuMobile = document.querySelector(menuMobile)
-        this.listaNav = document.querySelector(listaNav)
-        this.linksNav = document.querySelectorAll(linksNav)
-        this.classeAtiva = "ativa"
-        this.toqueClick = this.toqueClick.bind(this)
-    }
-
-    linksAnimados() {
-        this.linksNav.forEach((link, index) => {
-            link.style.animation
-                ? (link.style.animation = "")
-                : (link.style.animation = `linkNavOpaco 0.3s ease forwards ${
-                    index / 7 + 0.3
-                }s`)
-        })
-    }
-
-    toqueClick() {
-        this.listaNav.classList.toggle(this.classeAtiva)
-        this.menuMobile.classList.toggle(this.classeAtiva)
-        this.linksAnimados()
-    }
-
-    eventoClick() {
-        this.menuMobile.addEventListener("click", this.toqueClick)
-    }
-
-    inicializar() {
-        if (this.menuMobile) {
-            this.eventoClick()
+    class navBarMobile {
+        constructor(menuMobile, listaNav, linksNav) {
+            this.menuMobile = document.querySelector(menuMobile)
+            this.listaNav = document.querySelector(listaNav)
+            this.linksNav = document.querySelectorAll(linksNav)
+            this.classeAtiva = "ativa"
+            this.toqueClick = this.toqueClick.bind(this)
         }
-        return this
-    }
-}
 
-const navBarMobileFalsa = new navBarMobile(
-    ".menu-mobile",
-    ".lista-nav",
-    ".lista-nav li",
-)
-navBarMobileFalsa.inicializar()
+        linksAnimados() {
+            this.linksNav.forEach((link, index) => {
+                link.style.animation
+                    ? (link.style.animation = "")
+                    : (link.style.animation = `linkNavOpaco 0.3s ease forwards ${
+                        index / 7 + 0.3
+                    }s`)
+            })
+        }
+
+        toqueClick() {
+            this.listaNav.classList.toggle(this.classeAtiva)
+            this.menuMobile.classList.toggle(this.classeAtiva)
+            this.linksAnimados()
+        }
+
+        eventoClick() {
+            this.menuMobile.addEventListener("click", this.toqueClick)
+        }
+
+        inicializar() {
+            if (this.menuMobile) {
+                this.eventoClick()
+            }
+            return this
+        }
+    }
+
+    const navBarMobileFalsa = new navBarMobile(
+        ".menu-mobile",
+        ".lista-nav",
+        ".lista-nav li",
+    )
+    navBarMobileFalsa.inicializar()
 
 // Carrossel de imagens:
 
-const carrossel = document.querySelectorAll('.slider')
-const botaoVoltar = document.getElementById('botao-voltar')
-const botaoAvancar = document.getElementById('botao-avancar')
+    const carrossel = document.querySelectorAll('.slider')
+    const botaoVoltar = document.getElementById('botao-voltar')
+    const botaoAvancar = document.getElementById('botao-avancar')
 
-let imagemAtual = 0
+    let imagemAtual = 0
 
-function esconderImagem() {
-    carrossel.forEach(item => item.classList.remove('on'))
-}
-
-function mostrarImagem() {
-    carrossel[imagemAtual].classList.add('on')
-}
-
-function voltarImagem() {
-    esconderImagem()
-    if(imagemAtual === 0) {
-        imagemAtual = carrossel.length -1
-    } else {
-        imagemAtual--
+    function esconderImagem() {
+        carrossel.forEach(item => item.classList.remove('on'))
     }
-    mostrarImagem()
-}
 
-function avancarImagem() {
-    esconderImagem()
-    if(imagemAtual === carrossel.length -1) {
-        imagemAtual = 0
-    } else {
-        imagemAtual++
+    function mostrarImagem() {
+        carrossel[imagemAtual].classList.add('on')
     }
-    mostrarImagem()
-}
 
-botaoVoltar.addEventListener('click', voltarImagem)
-botaoAvancar.addEventListener('click', avancarImagem)
+    function voltarImagem() {
+        esconderImagem()
+        if(imagemAtual === 0) {
+            imagemAtual = carrossel.length -1
+        } else {
+            imagemAtual--
+        }
+        mostrarImagem()
+    }
+
+    function avancarImagem() {
+        esconderImagem()
+        if(imagemAtual === carrossel.length -1) {
+            imagemAtual = 0
+        } else {
+            imagemAtual++
+        }
+        mostrarImagem()
+    }
+
+    botaoVoltar.addEventListener('click', voltarImagem)
+    botaoAvancar.addEventListener('click', avancarImagem)
